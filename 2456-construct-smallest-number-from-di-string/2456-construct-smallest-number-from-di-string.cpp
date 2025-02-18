8575dec4fee4dde85676f3072b26148f62855ffd
Deepraj Singh <sengardeep2006@gmail.com>
2025-02-18 15:48:56 +0530

    Time: 0 ms (100%), Space: 8.6 MB (15.62%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     string smallestNumber(string pattern) {
-        string num = "";
-        for (int i = 0; i <= pattern.size(); i++) {
-            num += to_string(i + 1);
-        }
-        while (!check(num, pattern)) {
-            next_permutation(num.begin(),num.end());
-        }
-        return num;
-    }
+        string result = "";
+        stack<int> st;
 
-private:
-    bool check(string& num, string& pattern) {
-        for (int i = 0; i < pattern.size(); i++) {
-            if ((pattern[i] == 'I' && num[i] > num[i + 1]) ||
-                (pattern[i] == 'D' && num[i] < num[i + 1]))
-                return false;
+        for (int i = 0; i <= pattern.size(); i++) {
+            st.push(i + 1);
+
+            if (i == pattern.size() || pattern[i] == 'I') {
+                while (!st.empty()) {
+                    result += to_string(st.top());
+                    st.pop();
+                }
+            }
         }
-        return true;
+
+        return result;
     }
 };
