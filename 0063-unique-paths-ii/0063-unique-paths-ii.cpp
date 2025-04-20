c533b647c8e366d741f8002f1d42ad484d08956f
Deepraj Singh <sengardeep2006@gmail.com>
2025-04-20 15:30:11 +0530

    Time: 0 ms (100%), Space: 11.8 MB (71.13%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,21 +1,18 @@
 class Solution {
    public:
-    int uniquePathsWithObstacles(vector<vector<int>> &matrix) {
+    int uniquePathsWithObstacles(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return helper(matrix, dp, m - 1, n - 1);
-    }
+        if(matrix[0][0]==1) return 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 1) continue;
+                if (i - 1 >= 0) dp[i][j] += dp[i - 1][j];
+                if (j - 1 >= 0) dp[i][j] += dp[i][j - 1];
+            }
+        }
 
-   private:
-    int helper(vector<vector<int>> &matrix, vector<vector<int>> &dp, int row,
-               int col) {
-        if (row < 0 || col < 0 || matrix[row][col] == 1) return 0;
-        if (row == 0 && col == 0) return 1;
-        if (dp[row][col] != -1) return dp[row][col];
-        // Go for both directions (up and left)
-        int left = helper(matrix, dp, row - 1, col);
-        int up = helper(matrix, dp, row, col - 1);
-
-        return dp[row][col] = left + up;
+        return dp[m - 1][n - 1];
     }
 };
