de4b60924776f4d6ba8f33e2cb4899980d8d1836
Deepraj Singh <sengardeep2006@gmail.com>
2025-06-26 23:53:35 +0530

    Time: 4 ms (63.71%), Space: 11 MB (81.87%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if(numRows==1) return s;
-        int down=2*numRows-2,n=s.size();
-        string ans="";
-        int idx=0;
-        while(idx<n)
-        {
-            ans+=s[idx];
-            idx+=down;
-        }
-        int up=2;
-        down=down-2;
-        for(int i=1;i<numRows-1;i++)
-        {
-            int j=i;
-            bool flag=true;
-            while(j<n)
-            {
-                ans+=s[j];
-                if(flag)
-                {
-                    j+=down;
-                    flag=false;
-                }
-                else
-                {
-                    j+=up;
-                    flag=true;
+        if (numRows == 1)
+            return s;
+        int down = 2 * numRows - 2, n = s.size(), up = 0;
+        string ans = "";
+        for (int i = 0; i < numRows; i++) {
+            int j = i;
+            bool flag = true;
+            while (j < n) {
+                ans += s[j];
+                if (up == 0)
+                    flag = true;
+                if (down == 0)
+                    flag = false;
+                if (flag) {
+                    j += down;
+                    flag = false;
+                } else {
+                    j += up;
+                    flag = true;
                 }
             }
-            up+=2;
-            down-=2;
-        }
-        idx=numRows-1;
-        while(idx<n)
-        {
-            ans+=s[idx];
-            idx+=up;
+            up += 2;
+            down -= 2;
         }
 
         return ans;
