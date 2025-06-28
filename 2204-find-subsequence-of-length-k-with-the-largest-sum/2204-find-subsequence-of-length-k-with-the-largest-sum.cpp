041db96449840d47f1fe7e413b3c568ac937d577
Deepraj Singh <sengardeep2006@gmail.com>
2025-06-28 21:56:04 +0530

    Time: 0 ms (100%), Space: 13.6 MB (95.57%) - LeetHub

--- 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/2204-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -2,19 +2,33 @@ class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<pair<int, int>> vals;  // auxiliary array
-        for (int i = 0; i < n; ++i) {
-            vals.emplace_back(i, nums[i]);
+
+        if (k == n)
+            return nums;
+
+        vector<int> temp = nums;
+
+        // Partial Sort(O(n))
+        nth_element(
+            begin(temp), begin(temp) + k - 1, end(temp),
+            greater<int>()); // Fit the kth largest element on the kth position
+                             // and put greater on left and shorter on right
+
+        int countK = 0, el = temp[k - 1];
+        for (int i = 0; i < k; i++)
+            countK += (temp[i] == el);
+
+        temp.clear();
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > el)
+                temp.push_back(nums[i]);
+            else if (countK && nums[i] == el) {
+                countK--;
+                temp.push_back(nums[i]);
+            }
         }
-        // sort by numerical value in descending order
-        sort(vals.begin(), vals.end(),
-             [&](auto x1, auto x2) { return x1.second > x2.second; });
-        // select the first k elements and sort them in ascending order by index
-        sort(vals.begin(), vals.begin() + k);
-        vector<int> res;  // target subsequence
-        for (int i = 0; i < k; ++i) {
-            res.push_back(vals[i].second);
-        }
-        return res;
+
+        return temp;
     }
 };
