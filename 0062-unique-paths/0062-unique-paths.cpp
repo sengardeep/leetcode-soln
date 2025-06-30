6a66c8373bc82372149a8c3253e74491b4005e1d
Deepraj Singh <sengardeep2006@gmail.com>
2025-06-30 18:39:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (91.48%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,59 +1,10 @@
 class Solution {
-private:
-    //Function to solve the problem using space optimization.
-    int func(int m, int n){
-        /* Initialize a vector to represent 
-        the previous row of the grid.*/
-        vector<int> prev(n, 0);
-
-        // Iterate through the rows of the grid.
-        for (int i = 0; i < m; i++) {
-            /* Create a temporary vector to
-            represent the current row.*/
-            vector<int> temp(n, 0);
-
-            for (int j = 0; j < n; j++) {
-                // Base case
-                if (i == 0 && j == 0) {
-                    temp[j] = 1;
-                    continue;
-                }
-
-            /* Initialize variables to store the number
-            of ways from the cell above (up) and left (left).*/
-            int up = 0;
-            int left = 0;
-
-            /* If we are not at the first row (i > 0), update
-            'up' with the value from the previous row.*/
-            if (i > 0)
-                up = prev[j];
-
-            /* If we are not at the first column (j > 0),
-            update 'left' with the value from current row.*/
-            if (j > 0)
-                left = temp[j - 1];
-
-            /* Calculate the number of ways to reach the
-            current cell by adding 'up' and 'left'.*/
-            temp[j] = up + left;
-        }
-
-        /* Update the previous row with values 
-        calculated for the current row.*/
-        prev = temp;
-    }
-
-    /* The result is stored in the last
-    cell of the previous row (n-1).*/
-    return prev[n - 1];
-}
 public:
-    /*Function to count the total ways
-    to reach (0,0) from (m-1,n-1)*/
     int uniquePaths(int m, int n) {
-        
-        //Return the total count(0 based indexing)
-        return func(m, n);
+        long long ans = 1;
+        for (int i = 1; i <= m - 1; ++i) {
+            ans = ans * (n - 1 + i) / i;
+        }
+        return (int)ans;
     }
 };
