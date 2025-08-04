9b92515a207cceeeb8b252e0d301b6b40a7af23d
Deepraj Singh <sengardeep2006@gmail.com>
2025-08-04 10:34:45 +0530

    Time: 2017 ms (5.01%), Space: 48.9 MB (43.93%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,29 +1,32 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        int ans = helper(coins, n - 1, amount, dp);
-        return ans == 1e9 ? -1 : ans;
-    }
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,INT_MAX));
+        //dp[i][j] : min coins needed till i index to make j
+        for(int i=0;i<n;i++) dp[i][0]=0;
 
-private:
-    int helper(vector<int>& coins, int idx, int amount,
-               vector<vector<int>>& dp) {
-        if (amount == 0)
-            return 0;
-        if (idx == 0) {
-            if (amount % coins[idx] == 0)
-                return amount / coins[idx];
-            return 1e9;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                if(i==0){
+                if(j>=coins[i] && dp[i][j-coins[i]]!=INT_MAX)
+                dp[i][j]=min(dp[i][j],1+dp[i][j-coins[i]]);
+                }
+                else{
+                    if(j>=coins[i])
+                    {
+                        if(dp[i][j-coins[i]]!=INT_MAX)
+                            dp[i][j]=min(dp[i][j],1+dp[i][j-coins[i]]);
+                        if(dp[i-1][j-coins[i]]!=INT_MAX)
+                            dp[i][j]=min(dp[i][j],1+dp[i-1][j-coins[i]]);
+                        dp[i][j]=min(dp[i][j],dp[i-1][j]);
+                    }
+
+                    else if(j<coins[i]) dp[i][j]=dp[i-1][j];
+                }
+            }
         }
-        if (dp[idx][amount] != -1)
-            return dp[idx][amount];
-        int take = 1e9;
-        if (amount >= coins[idx])
-            take = 1 + helper(coins, idx, amount - coins[idx], dp);
-        int skip = helper(coins, idx - 1, amount, dp);
-
-        return dp[idx][amount] = min(take, skip);
+        for(auto it : dp[n-1]) cout<<it<<" ";
+        return (dp[n-1][amount]==INT_MAX)?-1:dp[n-1][amount];
     }
 };
