cebd943fc5b39b8d52a3f95a7a017a8456162764
Deepraj Singh <sengardeep2006@gmail.com>
2025-08-16 14:06:27 +0530

    Time: 0 ms (100%), Space: 13.2 MB (66.49%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -2,30 +2,22 @@ class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
-        vector<int> dp(n, -1);
-        return helper(days, costs, 0, n, dp);
-    }
+        vector<int> dp(n+1, INT_MAX);
+        dp[n]=0;
 
-private:
-    int helper(vector<int>& days, vector<int>& costs, int index, int n,
-               vector<int>& dp) {
-        if (index == n)
-            return 0;
-
-        if (dp[index] != -1)
-            return dp[index];
-        // 1-Day
-        int ans = costs[0] + helper(days, costs, index + 1, n, dp);
-        // 7-Days
-        int num = days[index] + 6;
-        int idx = upper_bound(days.begin(), days.end(), num) - days.begin();
-        ans = min(ans, costs[1] + helper(days, costs, idx, n, dp));
-
-        // 30-Days
-        num = days[index] + 29;
-        idx = upper_bound(days.begin(), days.end(), num) - days.begin();
-        ans = min(ans, costs[2] + helper(days, costs, idx, n, dp));
-
-        return dp[index] = ans;
+        for(int i=n-1;i>=0;i--)
+        {
+            //1-Day
+            dp[i]=min(dp[i],costs[0]+dp[i+1]);
+            //7-Days
+            int num = days[i] + 6;
+            int idx = upper_bound(days.begin(), days.end(), num) - days.begin();
+            dp[i]=min(dp[i],costs[1]+dp[idx]);
+            //30-Days
+            num = days[i] + 29;
+            idx = upper_bound(days.begin(), days.end(), num) - days.begin();
+            dp[i]=min(dp[i],costs[2]+dp[idx]);
+        }
+        return dp[0];
     }
 };
