b3e868a3ccf7238e3b78f5e77e7522d7f480bab0
Deepraj Singh <sengardeep2006@gmail.com>
2025-08-22 19:08:20 +0530

    Time: 23 ms (15.46%), Space: 13 MB (48.6%) - LeetHub

--- 1989-minimum-xor-sum-of-two-arrays/1989-minimum-xor-sum-of-two-arrays.cpp
+++ 1989-minimum-xor-sum-of-two-arrays/1989-minimum-xor-sum-of-two-arrays.cpp
@@ -3,26 +3,27 @@ public:
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
         int n = nums2.size();
         int p = 1 << n;
-        vector<vector<int>> dp(n, vector<int>(p, -1));
+        vector<int> dp(p, -1);
 
-        function<int(int, int)> f = [&](int index, int mask) {
+        function<int(int)> f = [&](int mask) {
+            int index = __builtin_popcount(mask);
             if (index == n)
                 return 0;
-            if (dp[index][mask] != -1)
-                return dp[index][mask];
+            if (dp[mask] != -1)
+                return dp[mask];
 
             int ans = INT_MAX;
 
             for (int i = 0; i < n; i++) {
                 if ((mask & (1 << i)) == 0) {
-                    ans = min(ans, (nums1[index] ^ nums2[i]) +
-                                       f(index + 1, (mask | (1 << i))));
+                    ans = min(ans,
+                              (nums1[index] ^ nums2[i]) + f((mask | (1 << i))));
                 }
             }
 
-            return dp[index][mask] = ans;
+            return dp[mask] = ans;
         };
 
-        return f(0, 0);
+        return f(0);
     }
 };
