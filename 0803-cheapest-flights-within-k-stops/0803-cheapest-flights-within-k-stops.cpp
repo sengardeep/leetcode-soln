9cad7f82508ba957aec435845d9e1c7f4222bcee
Deepraj Singh <sengardeep2006@gmail.com>
2025-09-18 19:00:12 +0530

    Time: 4 ms (45.21%), Space: 18.8 MB (21.88%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst,
-                          int k) {
-
-        // Shortest path algorithm making priority of number of stops
-        vector<pair<int, int>> adj[n];
-        for (auto it : flights) {
-            adj[it[0]].push_back({it[1], it[2]});
-        }
-        queue<tuple<int, int, int>> q; //(stops,node,distance)
-        q.push({0, src, 0});
-        vector<int> ans(n, INT_MAX);
-        ans[src] = 0;
-        // vector<bool> visited(n, 0);
-        while (!q.empty()) {
-            int stops = get<0>(q.front());
-            int node = get<1>(q.front());
-            int dist = get<2>(q.front());
-            // if (node != dst)
-            //     visited[node] = true;
-                q.pop();
-            for (int i = 0; i < adj[node].size(); i++) {
-                int nbr = adj[node][i].first;
-                int d = adj[node][i].second;
-
-                if ((nbr == dst && stops > k) || d + dist >= ans[nbr])
-                    continue;
-
-                ans[nbr] = min(ans[nbr], d + dist);
-                q.push({stops + 1, nbr, ans[nbr]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        using pii = pair<int,int>;
+        vector<vector<pii>> adj(n);
+        for(auto it : flights) adj[it[0]].push_back({it[1],it[2]});
+        using T = tuple<int,int,int>;
+        priority_queue<T,vector<T>,greater<T>> pq;
+        pq.push({0,src,0}); //{cost,node,k}
+        vector<long long> ans(n,1e18);
+        ans[src]=0;
+        while(!pq.empty()){
+            auto [x,node,c] = pq.top(); pq.pop();
+            // if(ans[node] < c) continue;
+            if(x>k) continue;
+            for(auto [child,w] : adj[node]){
+                if(ans[child] > w+c){
+                    ans[child]=w+c;
+                    pq.push({x+1,child,ans[child]});
+                }
             }
         }
-
-        if (ans[dst] == INT_MAX)
-            return -1;
-        return ans[dst];
+        // for(auto it : ans) cout<<it<<" ";
+        return (ans[dst]==1e18)?-1:ans[dst];
     }
 };
