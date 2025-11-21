b40b73bf7593f03cc8e6aa6ad7255e6700c386f1
Deepraj Singh <sengardeep2006@gmail.com>
2025-11-21 21:28:38 +0530

    Time: 1 ms (58.78%), Space: 10.7 MB (24.32%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return 0;
+        string s=to_string(x);
+        int i=0,j=s.size()-1;
+        while(i<j){
+            if(s[i]==s[j]){
+                i++;
+                j--;
+            }else return 0;
+        }
+        return 1;
+    }
+};
