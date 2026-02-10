f167fc0de6b6a15cfd365abc6f0efb6390e70e46
Deepraj Singh <sengardeep2006@gmail.com>
2026-02-10 07:06:40 +0530

    Time: 2071 ms (24.95%), Space: 473.1 MB (71.28%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int ans=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            set<int> even,odd;
+            for(int j=i;j<n;j++) {
+                if(nums[j]%2) odd.insert(nums[j]);
+                else even.insert(nums[j]);
+                if(even.size()==odd.size()) ans=max(ans,j-i+1);
+            }
+        }   
+        return ans;
+    }
+};
