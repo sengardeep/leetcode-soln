9d5b4193e97a3519cba06880ff60d9402a191b93
Deepraj Singh <sengardeep2006@gmail.com>
2026-08-06 10:18:16 +0530

    Time: 20 ms (64.52%), Space: 85.6 MB (44.74%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        vector<int> nsl(heights.size(), -1); // Nearest Smallest on left
-        vector<int> nsr(heights.size(),
-                        heights.size()); // Nearest Smallest on right
-
-        // NSL
-        stack<int> st1;
-        for (int i = 0; i < heights.size(); i++) {
-            while ((!st1.empty()) && heights[st1.top()] >= heights[i])
-                st1.pop();
-
-            if (!(st1.empty()))
-                nsl[i] = st1.top();
-            st1.push(i);
+        int n=heights.size();
+        vector<int> nsl(n,-1),nsr(n,n);
+        //left
+        stack<int> stack;
+        for(int i=0;i<n;i++){
+            while(!stack.empty() && heights[stack.top()]>=heights[i]) stack.pop();
+            if(!stack.empty()) nsl[i]=stack.top();
+            stack.push(i);
+        }
+        while(!stack.empty()) stack.pop();
+        //right
+        for(int i=n-1;i>=0;i--){
+            while(!stack.empty() && heights[stack.top()]>=heights[i]) stack.pop();
+            if(!stack.empty()) nsr[i]=stack.top();
+            stack.push(i);
         }
 
-        // NSR
-        stack<int> st2;
-        for (int i = heights.size() - 1; i >= 0; i--) {
-            while ((!st2.empty()) && heights[st2.top()] >= heights[i])
-                st2.pop();
-
-            if (!(st2.empty()))
-                nsr[i] = st2.top();
-            st2.push(i);
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int h=heights[i],w=nsr[i]-nsl[i]-1;
+            ans=max(ans,h*w);
         }
-
-        long long ans = INT_MIN;
-
-        for (int i = 0; i < heights.size(); i++) {
-            int maxWidth = nsr[i] - nsl[i] - 1;
-            long long maxArea = heights[i] * maxWidth;
-            ans = max(ans, maxArea);
-        }
-
-        return (int)ans;
+        return ans;
     }
 };
