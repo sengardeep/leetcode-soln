a26ea78138ece1df9ea4f750f79207d75fa8157a
Deepraj Singh <sengardeep2006@gmail.com>
2026-08-08 18:24:26 +0530

    Time: 50 ms (95.36%), Space: 148.5 MB (93.04%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,27 +1,30 @@
 class MedianFinder {
 public:
+    priority_queue<int> pq1;
+    priority_queue<int, vector<int>, greater<int>> pq2;
     MedianFinder() {}
-    priority_queue<int> left;
-    priority_queue<int, vector<int>, greater<int>> right;
+
     void addNum(int num) {
-        if (left.empty() || num < left.top()) {
-            left.push(num);
-        } else
-            right.push(num);
-        if (left.size() > right.size() + 1) {
-            right.push(left.top());
-            left.pop();
-        } else if (right.size() > left.size()) {
-            left.push(right.top());
-            right.pop();
+        if (pq1.empty() || num <= pq1.top())
+            pq1.push(num);
+        else
+            pq2.push(num);
+        if (pq2.size() > pq1.size()) {
+            pq1.push(pq2.top());
+            pq2.pop();
+        } else if (pq1.size() > pq2.size() + 1) {
+            pq2.push(pq1.top());
+            pq1.pop();
         }
     }
 
     double findMedian() {
-        if (left.size() == right.size()) {
-            return (double)((double)(left.top() + right.top()) / 2);
-        } else
-            return (double)(left.top());
+        int total = pq1.size() + pq2.size();
+        if (total & 1) {
+            return pq1.top();
+        } else {
+            return ((double)(pq1.top() + pq2.top())) / 2;
+        }
     }
 };
 
