55896a2063ab5bcc6b7ca06f87bf12396fbdecdc
Deepraj Singh <sengardeep2006@gmail.com>
2026-08-09 20:01:35 +0530

    Time: 83 ms (18.87%), Space: 112.6 MB (17.25%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-    void generate(vector<int>& arr, int idx, vector<vector<int>>& ans, int k,
-                  vector<int>& temp) {
-        if (idx == arr.size()) {
-            if (temp.size() == k) {
-                ans.push_back(temp);
-            }
-            return;
-        }
-        temp.push_back(arr[idx]);
-        generate(arr, idx + 1, ans, k, temp);
-        temp.pop_back();
-
-        generate(arr, idx + 1, ans, k, temp);
-    }
     vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> ans;
-        vector<int> nums;
-        for (int i = 1; i <= n; i++)
-            nums.push_back(i);
         vector<int> temp;
-        generate(nums, 0, ans, k, temp);
+        function<void(int)> f=[&](int i){
+            if(temp.size()==k) {
+                ans.push_back(temp);
+                return;
+            }
+            if(i>n) return;
+            temp.push_back(i);
+            f(i+1);
+            temp.pop_back();
+            f(i+1);
+        };
+        f(1);
         return ans;
     }
 };
