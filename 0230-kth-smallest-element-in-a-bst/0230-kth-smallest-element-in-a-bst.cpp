d60c5cf80fba0bbe33e8b0f89d5b7dbfc3665c1a
Deepraj Singh <sengardeep2006@gmail.com>
2026-08-26 23:15:07 +0530

    Time: 0 ms (100%), Space: 24.5 MB (40.64%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -6,33 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    int count = 0; // Counter for visited nodes
-
     int kthSmallest(TreeNode* root, int k) {
-        TreeNode* result = helper(root, k);
-        return result ? result->val : 0; // Return value or 0 if not found
-    }
-
-    TreeNode* helper(TreeNode* root, int k) {
-        if (root == nullptr)
-            return nullptr;
-
-        // Traverse left subtree
-        TreeNode* left = helper(root->left, k);
-        if (left != nullptr)
-            return left; // If found in left subtree
-
-        count++; // Increment count for current node
-        if (count == k)
-            return root; // Found k-th smallest
-
-        // Traverse right subtree
-        return helper(root->right, k);
+        int ans=0;
+        function<void(TreeNode*)> f = [&](TreeNode* root){
+            if(!root) return;
+            f(root->left);
+            if(k>1) k--;
+            else if(k==1){
+                k--;
+                ans=root->val;
+                return;
+            }
+            f(root->right);
+        };
+        f(root);
+        return ans;
     }
 };
