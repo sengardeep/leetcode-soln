119c0408f60bad20fe0bbad202ffbfed9ced300f
Deepraj Singh <sengardeep2006@gmail.com>
2026-08-27 19:01:52 +0530

    Time: 32 ms (79.06%), Space: 53.4 MB (92.65%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
     vector<vector<string>> partition(string s) {
-        vector<string> temp;
-        vector<vector<string>> ans;
-        // string res;
-        solve(s, 0, temp, ans);
-        return ans;
-    }
-
-private:
-    // Function to check if a string is palindrome
-    bool isPalindrome(const string& s, int left, int right) {
-        while (left < right) {
-            if (s[left++] != s[right--])
-                return false;
-        }
-        return true;
-    }
-
-    // Backtracking function
-    void solve(string& s, int idx, vector<string>& temp,
-               vector<vector<string>>& ans) {
-        if (idx == s.size()) {
-            ans.push_back(temp);
-            return;
-        }
-
-        for (int i = idx; i < s.size(); i++) {
-            if (isPalindrome(s, idx, i)) {
-                temp.push_back(s.substr(idx, i - idx + 1));
-                solve(s, i + 1, temp, ans);
-                temp.pop_back();
+        auto check=[&](int l,int r){
+            while(l<=r){
+                if(s[l]!=s[r]) return 0;
+                l++;
+                r--;
             }
-        }
+            return 1;
+        };
+        int n=s.size();
+        vector<vector<string>> ans;
+        vector<string> temp;
+        function<void(int)> f=[&](int index){
+            if(index==n) {
+                ans.push_back(temp);
+                return;
+            }
+            for(int i=index;i<n;i++){
+                if(check(index,i)){
+                    string t = s.substr(index,i-index+1);
+                    temp.push_back(t);
+                    f(i+1);
+                    temp.pop_back();
+                }
+            }
+        };
+        f(0);
+        return ans;
     }
 };
