616511c145ae446c0b8f3b6ba0b4e4ed4c0e4f48
Deepraj Singh <sengardeep2006@gmail.com>
2026-09-08 12:03:32 +0530

    Time: 12 ms (8.43%), Space: 18.4 MB (57.63%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> indeg(numCourses, 0);
         vector<int> adj[numCourses];
-        vector<int> inDegree(numCourses,0);
-
-        for(auto it : prerequisites)
-        {
-            adj[it[1]].push_back(it[0]);
-            inDegree[it[0]]++;
+        for (auto v : prerequisites) {
+            indeg[v[0]]++;
+            adj[v[1]].push_back(v[0]);
         }
-
         queue<int> q;
-        for(int i=0;i<numCourses;i++) if(inDegree[i]==0) q.push(i);
-
+        for (int i = 0; i < numCourses; i++) {
+            if (indeg[i] == 0) q.push(i);
+        }
         vector<int> ans;
-        while(!q.empty())
-        {
-            int node=q.front();
+        while(!q.empty()){
+            auto node = q.front();
             ans.push_back(node);
             q.pop();
-            for(int i=0;i<adj[node].size();i++)
-            {
-                int neighbour=adj[node][i];
-                inDegree[neighbour]--;
-                if(inDegree[neighbour]==0) q.push(neighbour);
+            for(auto neighb : adj[node]){
+                indeg[neighb]--;
+                if(indeg[neighb]==0){
+                    q.push(neighb);
+                }
             }
         }
-        if(ans.size()!=numCourses) return {};
+        for(auto x : indeg) if(x) return {};
         return ans;
     }
 };
